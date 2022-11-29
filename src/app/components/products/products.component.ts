19d4093f8bb10191595361e8d3345dc4f576ba4f
Escalos <stornblood6969@gmail.com>
2022-11-28 22:40:32 -0500

    Terminando de usar pipes

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Product } from '../../models/product.model'
+import { StoreService } from '../../services/store.service';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -7,258 +9,28 @@ import { Product } from '../../models/product.model'
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent {
-  total = 0;
   myShoppingCart: Product[] = [];
+  total: number = 0;
+  products: Product[] = [];
+  today = new Date();
+  data = new Date(2021,1,21);
 
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'Rem & Ram',
-      price: 150,
-      image: './assets/images/rem_ram.jpg',
-    },
-    {
-      id: '2',
-      name: 'Megumi Katou',
-      price: 50,
-      image: './assets/images/megumi.jpg',
-    },
-    {
-      id: '3',
-      name: 'Yami',
-      price: 100,
-      image: './assets/images/yami.jpg',
-    },
-    {
-      id: '1',
-      name: 'Random',
-      price: 250,
-      image: './assets/images/imouto.jpg',
-    },
-    {
-      id: '1',
-      name: 'Rem & Ram',
-      price: 150,
-      image: './assets/images/rem_ram.jpg',
-    },
-    {
-      id: '2',
-      name: 'Megumi Katou',
-      price: 150,
-      image: './assets/images/megumi.jpg',
-    },
-    {
-      id: '3',
-      name: 'Yami',
-      price: 150,
-      image: './assets/images/yami.jpg',
-    },
-    {
-      id: '1',
-      name: 'Random',
-      price: 150,
-      image: './assets/images/imouto.jpg',
-    },
-    {
-      id: '1',
-      name: 'Rem & Ram',
-      price: 150,
-      image: './assets/images/rem_ram.jpg',
-    },
-    {
-      id: '2',
-      name: 'Megumi Katou',
-      price: 150,
-      image: './assets/images/megumi.jpg',
-    },
-    {
-      id: '3',
-      name: 'Yami',
-      price: 150,
-      image: './assets/images/yami.jpg',
-    },
-    {
-      id: '1',
-      name: 'Random',
-      price: 150,
-      image: './assets/images/imouto.jpg',
-    },
-    {
-      id: '1',
-      name: 'Rem & Ram',
-      price: 150,
-      image: './assets/images/rem_ram.jpg',
-    },
-    {
-      id: '2',
-      name: 'Megumi Katou',
-      price: 150,
-      image: './assets/images/megumi.jpg',
-    },
-    {
-      id: '3',
-      name: 'Yami',
-      price: 150,
-      image: './assets/images/yami.jpg',
-    },
-    {
-      id: '1',
-      name: 'Random',
-      price: 150,
-      image: './assets/images/imouto.jpg',
-    },
-    {
-      id: '1',
-      name: 'Rem & Ram',
-      price: 150,
-      image: './assets/images/rem_ram.jpg',
-    },
-    {
-      id: '2',
-      name: 'Megumi Katou',
-      price: 150,
-      image: './assets/images/megumi.jpg',
-    },
-    {
-      id: '3',
-      name: 'Yami',
-      price: 150,
-      image: './assets/images/yami.jpg',
-    },
-    {
-      id: '1',
-      name: 'Random',
-      price: 150,
-      image: './assets/images/imouto.jpg',
-    },
-    {
-      id: '1',
-      name: 'Rem & Ram',
-      price: 150,
-      image: './assets/images/rem_ram.jpg',
-    },
-    {
-      id: '2',
-      name: 'Megumi Katou',
-      price: 150,
-      image: './assets/images/megumi.jpg',
-    },
-    {
-      id: '3',
-      name: 'Yami',
-      price: 150,
-      image: './assets/images/yami.jpg',
-    },
-    {
-      id: '1',
-      name: 'Random',
-      price: 150,
-      image: './assets/images/imouto.jpg',
-    },
-    {
-      id: '1',
-      name: 'Rem & Ram',
-      price: 150,
-      image: './assets/images/rem_ram.jpg',
-    },
-    {
-      id: '2',
-      name: 'Megumi Katou',
-      price: 150,
-      image: './assets/images/megumi.jpg',
-    },
-    {
-      id: '3',
-      name: 'Yami',
-      price: 150,
-      image: './assets/images/yami.jpg',
-    },
-    {
-      id: '1',
-      name: 'Random',
-      price: 150,
-      image: './assets/images/imouto.jpg',
-    },
-    {
-      id: '1',
-      name: 'Rem & Ram',
-      price: 150,
-      image: './assets/images/rem_ram.jpg',
-    },
-    {
-      id: '2',
-      name: 'Megumi Katou',
-      price: 150,
-      image: './assets/images/megumi.jpg',
-    },
-    {
-      id: '3',
-      name: 'Yami',
-      price: 150,
-      image: './assets/images/yami.jpg',
-    },
-    {
-      id: '1',
-      name: 'Random',
-      price: 150,
-      image: './assets/images/imouto.jpg',
-    },
-    {
-      id: '1',
-      name: 'Rem & Ram',
-      price: 150,
-      image: './assets/images/rem_ram.jpg',
-    },
-    {
-      id: '2',
-      name: 'Megumi Katou',
-      price: 150,
-      image: './assets/images/megumi.jpg',
-    },
-    {
-      id: '3',
-      name: 'Yami',
-      price: 150,
-      image: './assets/images/yami.jpg',
-    },
-    {
-      id: '1',
-      name: 'Random',
-      price: 150,
-      image: './assets/images/imouto.jpg',
-    },
-    {
-      id: '1',
-      name: 'Rem & Ram',
-      price: 150,
-      image: './assets/images/rem_ram.jpg',
-    },
-    {
-      id: '2',
-      name: 'Megumi Katou',
-      price: 150,
-      image: './assets/images/megumi.jpg',
-    },
-    {
-      id: '3',
-      name: 'Yami',
-      price: 150,
-      image: './assets/images/yami.jpg',
-    },
-    {
-      id: '1',
-      name: 'Random',
-      price: 150,
-      image: './assets/images/imouto.jpg',
-    },
-  ];
+  constructor(
+    private storeService: StoreService,
+    private productsService: ProductsService,
+  ) {
+    this.myShoppingCart = this.storeService.getMyShoppingCart();
+  }
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.productsService.getAllProducts()
+    .subscribe(data => {
+      this.products = data;
+    });
+  }
 
   onAddToShoppingCart(product: Product) {
-    this.myShoppingCart.push(product);
-    this.total = this.myShoppingCart.reduce((sum,item) => sum + item.price, 0);
+    this.storeService.addProduct(product);
+    this.total = this.storeService.getTotal();
   }
 }
